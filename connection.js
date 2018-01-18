0bb8cad4f614153b3ce4926d83328cde15e30bb3
Stephenson <dfstephenson@labs.neit.edu>
2018-01-18 12:55:02 -0500

    Updated connection.js

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db){
 	if(err) throw err;
-	console.log("Connected to mongoDB");
+	console.log("Connected to mongoDB Hello World");
 	
 	db.close();
 	
